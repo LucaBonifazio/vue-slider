c22200f6b0689862cd853378d550933f35ae131b
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-11-15 11:19:52 +0100

    add timeout

--- js/script.js
+++ js/script.js
@@ -1,96 +1,80 @@
-    new Vue({
-        el: '.background',
-        data: {
-            arrImages: [
-                {
-                    image: '01.webp',
-                    title: "Marvel's Spiderman Miles Morale",
-                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                },
-                {
-                    image: '02.webp',
-                    title: 'Ratchet & Clank: Rift Apart',
-                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                },
-                {
-                    image: '03.webp',
-                    title: 'Fortnite',
-                    text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
-                },
-                {
-                    image: '04.webp',
-                    title: 'Stray',
-                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                },
-                {
-                    image: '05.webp',
-                    title: "Marvel's Avengers",
-                    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-                },
-            ],
-
-            timeSlider: 3000,
-            direction: 1,
-            activeIndex: 0,
-            idInterval: 0,
-            isAutoplayActive: true,
-        },
-        methods: {
-
-            moveSlide(direction) {
-                if (direction > 0) {
-                    this.activeIndex++;
-                    if (this.activeIndex === this.arrImages.length) {
-                        this.activeIndex = 0;
-                    }
-                } else {
-                    if (this.activeIndex === 0) {
-                        this.activeIndex = this.arrImages.length;
-                    }
-                    this.activeIndex--;
-                }
+new Vue({
+    el: '.background',
+    data: {
+        arrImages: [
+            {
+                image: '01.webp',
+                title: "Marvel's Spiderman Miles Morale",
+                text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
             },
+            {
+                image: '02.webp',
+                title: 'Ratchet & Clank: Rift Apart',
+                text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+            },
+            {
+                image: '03.webp',
+                title: 'Fortnite',
+                text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+            },
+            {
+                image: '04.webp',
+                title: 'Stray',
+                text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+            },
+            {
+                image: '05.webp',
+                title: "Marvel's Avengers",
+                text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+            },
+        ],
 
-            // startAutoplay() {
-            //     this.idInterval = this.setInterval(() => this.moveSlide(direction), this.timeSlider);
-            // },
+        timeSlider: 3000,
+        direction: 1,
+        activeIndex: 0,
+        idInterval: 0,
+        isAutoplayActive: true,
+    },
+    methods: {
 
-        //     setInterval() {
-        //         (this.moveSlide(direction), this.timeSlider);  
-        //     }, 
-            
-        //     stopAutoplay() {
-        //         clearInterval(this.idInterval);
-        //     },
-            
-        //     invertDirection() {
-        //         this.direction *= -1;
-        //     },
+        moveSlide(direction) {
+            if (direction > 0) {
+                this.activeIndex++;
+                if (this.activeIndex === this.arrImages.length) {
+                    this.activeIndex = 0;
+                }
+            } else {
+                if (this.activeIndex === 0) {
+                    this.activeIndex = this.arrImages.length;
+                }
+                this.activeIndex--;
+            }
+        },
 
-        //    mouseenter() {
-        //     this.stopAutoplay()
-        //    },
+        clickActiveIndex(index) {
+            this.activeIndex = index;
+        },
 
-        //     mouseleave() {
-        //         if (this.isAutoplayActive) {
-        //             this.startAutoplay();
-        //         }
-        //     },
+        controlAutoplay() {
+            if (this.isAutoplayActive) {
+                this.stopAutoplay();
+            } else {
+                this.startAutoplay();
+            }
+        },
 
-        //     reverseButton() {
-        //         this.invertDirection();
-        //     }, 
+        startAutoplay() {
+            this.idInterval = setInterval(() => this.moveSlide(this.direction), this.timeSlider);
+            this.isAutoplayActive = true;
+        },
 
-        //     startStop() {
-        //         if (isAutoplayActive) {
-        //             this.stopAutoplay();
-        //             isAutoplayActive = false;
-        //             this.innerHTML = 'Start';
-        //         } else {
-        //             this.startAutoplay();
-        //             isAutoplayActive = true;
-        //             this.innerHTML = 'Stop';
-        //         }
-        //     },
-        }            
-    });
+        stopAutoplay() {
+            clearInterval(this.idInterval);
+            this.isAutoplayActive = false;
+        },
+
+        invertAutoPlay() {
+            this.direction *+ -1
+        },
+    }
+});
